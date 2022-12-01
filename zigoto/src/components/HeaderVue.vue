114bb21d57520b42a55e7f66c161b461b37d3950
Jeremy3333 <jeremy.simond220801@gmail.com>
2022-12-01 21:25:47 +0100

    Added header

--- zigoto/src/components/HeaderVue.vue
+++ zigoto/src/components/HeaderVue.vue
@@ -1,5 +1,31 @@
 <template>
     <header>
+        <img :src="require('../assets/img/ISTV.png')">
         <h1>IST en folie</h1>
     </header>
 </template>
+
+<style>
+header {
+    width: 100%;
+    height: 100px;
+    background-color: #FA97CD;
+    text-align: center;
+    display: flex;
+    flex-direction: row;
+}
+
+img {
+    width: 100px;
+    height: 100px;
+    margin-left: 3%;
+}
+
+h1 {
+    color: white;
+    font-size: 3em;
+    margin-top: auto;
+    margin-bottom: auto;
+    width: 90%;
+}
+</style>
