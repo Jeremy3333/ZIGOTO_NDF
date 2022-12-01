c57d772cc6688162bc03643eb1a3f463d8f8862c
Jeremy3333 <jeremy.simond220801@gmail.com>
2022-12-01 23:07:34 +0100

    reset

--- zigoto/src/components/NavVue.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <nav>
-        <ul>
-            <li><router-link to="/">Accueil</router-link></li>
-            <li><router-link to="/about">A propos</router-link></li>
-            <li><router-link to="/contact">Contact</router-link></li>
-        </ul>
-    </nav>
-</template>
